6699381d12af170395bd673307a8c2b1cc52fd07
Andres Puello Osorio <andrespuello53@gmail.com>
2024-10-03 13:47:19 -0500

    add chart to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
-import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { CookieService } from 'ngx-cookie-service';
 
@@ -21,8 +21,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { AppComponent } from './app.component';
 
 import { errorInterceptor } from './core/services/error.service';
-import { tokenInterceptor } from './core/services/token.interceptor';
 import { spinnerInterceptor } from './core/services/spinner.service';
+import { tokenInterceptor } from './core/services/token.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
